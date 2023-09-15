42da1a80fb55edc7f8345a697dc1a748051920c3
PrimusAbOris <guy.ross.rit@gmail.com>
2023-09-15 11:26:50 -0400

    Split inventory output to two steps with a yes/no prompt in between

--- src/main/FarmerGuyBarnyardInventoryApplication.java
+++ src/main/FarmerGuyBarnyardInventoryApplication.java
@@ -1,19 +1,31 @@
 package main;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 import animals.*;
 
 public class FarmerGuyBarnyardInventoryApplication {
 
-	public static void main(String[] args) {
-		// New solution
-		
+	public static void main(String[] args) throws IOException, InterruptedException {		
 		Animal[] animals = {new Cow(), new Pig(), new Sheep(),
 				new Snorlax(""), new Snorlax("awake"), new Articuno()};
 		String[] names = {"Cow", "Pig", "Sheep", "Snorlax", "Borlax", "Articuno"};
-		Snorlax Borlax = (Snorlax)animals[4];
+		// Snorlax Borlax = (Snorlax)animals[4];    not used yet
+		
+		System.out.println("The animals currently in your inventory are:");
+		for (String n : names) {
+			System.out.print(n + " | ");
+		}
+		System.out.println();
+		
+		if (!askYN()) {
+			System.out.println("Sorry to hear that, but that isn't handled yet.");
+			Thread.sleep((Snorlax.maxHP)*4);
+			System.out.println();
+		}
 		
 		int i = 0;
-		
 		for (Animal a : animals) {
 			System.out.println(names[i] + ":");
 			a.animalSound();
@@ -22,7 +34,24 @@ public class FarmerGuyBarnyardInventoryApplication {
 			i++;
 		}
 
-		System.out.println(Snorlax.maxHP + " " + Borlax.maxHP + " " + Articuno.maxHP);
+		System.out.println(Snorlax.maxHP + " " + Snorlax.maxHP + " " + Articuno.maxHP);
 		Articuno.sheerCold(8);
+	}
+	
+	public static boolean askYN() throws InterruptedException {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Alright? (yes/no): ");
+		String input = sc.nextLine().toLowerCase(); // not case sensitive
+		sc.close();
+
+		if (input.equals("yes")) return true;
+		else if (input.equals("no")) return false;
+		else {
+			System.out.println("Invalid input. I take it as a yes!");
+			Thread.sleep((Snorlax.maxHP)*4);
+			return true;
+		}
+
 	}
 }
